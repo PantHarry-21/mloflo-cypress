d48008d0176ba759a45f5d4fa65657303721e6f8
pantharry-21 <himanshupant.qa@gmail.com>
2025-02-20 00:07:04 +0530

    New files added

---
+++ e2e/tests/addPartnerLead.cy.js
@@ -0,0 +1,73 @@
+const { faker } = require("@faker-js/faker");
+
+// Custom command for login
+Cypress.Commands.add("login", (email, password) => {
+    cy.get("input[name='email']").clear().type(email); // Enter email
+    cy.get("input[placeholder='Password']").clear().type(password); // Enter password
+    cy.get("#submit").click(); // Click submit button
+});
+
+describe("Add Partner Lead from Pipeline - Optimized", () => {
+    beforeEach(() => {
+        cy.visit("https://uat.mloflo.com/"); // Navigate to application URL
+    });
+
+    it("Adds a partner lead from pipeline", () => {
+        cy.login("himanshupant.qa@gmail.com", "Harry@123"); // Login with valid credentials
+        cy.url().should("include", "/dashboard"); // Verify successful login
+        
+        cy.get('div > .menu-logout > li:nth-child(2) > a > .uil-chart')
+          .should('be.visible')
+          .click(); // Navigate to Pipeline page
+        
+        cy.url().should("include", "/pipeline"); // Verify navigation to pipeline
+        
+        cy.get('.layout-grid-height-1 > .mileStone > .mileStone-tab > .nav-item:nth-child(3) > .nav-link')
+          .should('be.visible')
+          .click(); // Navigate to Partner Leads tab
+        
+        cy.get('.form-row > .d-flex > .position-relative > .d-flex > .button')
+          .should('be.visible')
+          .click(); // Click on Add Partner Lead button
+        
+        // Fill partner lead details
+        cy.get('#modalBody > .form-row > .form-group:nth-child(1) > .selectDiv > .champ-form__cm-input')
+          .should('be.visible')
+          .select('RT'); // Select Role Type
+        
+        cy.get('#modalBody > .form-row > .form-group:nth-child(2) > .selectDiv > .champ-form__cm-input')
+          .should('be.visible')
+          .select('PC'); // Select Partner Category
+        
+        cy.get('#modalBody > .form-row > .cstm-light-text:nth-child(3) > .selectDiv > .champ-form__cm-input')
+          .should('be.visible')
+          .select('selfgen'); // Select Source
+        
+        cy.get('form > #modalBody > .form-row > .cstm-light-text:nth-child(5) > .champ-form__cm-input')
+          .should('be.visible')
+          .type(faker.person.firstName()); // Enter First Name
+        
+        cy.get('form > #modalBody > .form-row > .form-group:nth-child(6) > .champ-form__cm-input')
+          .should('be.visible')
+          .type(faker.person.lastName()); // Enter Last Name
+        
+        cy.get('form > #modalBody > .form-row > .form-group:nth-child(7) > .champ-form__cm-input')
+          .should('be.visible')
+          .type(faker.internet.email()); // Enter Email
+        
+        cy.get('form > #modalBody > .form-row > .form-group:nth-child(8) > .champ-form__cm-input')
+          .should('be.visible')
+          .type(faker.phone.number()); // Enter Phone Number
+        
+        cy.get('form > #modalBody > .form-row > .form-group:nth-child(12) > .champ-form__cm-input')
+          .should('be.visible')
+          .type(faker.company.name()); // Enter Company Name
+        
+        cy.get('.modal-box--dialog__container-footer > .w-100 > .position-relative:nth-child(3) > .button > span')
+          .should('be.enabled')
+          .click(); // Click Submit Button
+        
+        cy.get('.Snackbar_snackbar__text__1Hx2a')
+          .should("contain", "Partner Created successfully"); // Verify success message
+    });
+});
--- e2e/tests/test scenarios/recTest.cy.js
+++ e2e/tests/test scenarios/recTest.cy.js
@@ -2,25 +2,22 @@ describe('test_name', function() {
 
    it('what_it_does', function() {
   
-      cy.viewport(1280, 631)
+      cy.viewport(1280, 678)
    
-      cy.visit('https://stage.mloflo.com/loan-officer/dashboard')
+      cy.visit('https://uat.mloflo.com/loan-officer/contacts')
    
-      cy.get('form > #modalBody > .form-row > .form-group > #auto--select\ undefined').click()
+      cy.get('.layout-grid-height-1 > .mileStone > .mileStone-tab > .nav-item:nth-child(2) > .nav-link').click()
    
-      cy.get('#modalBody > .form-row > .col-sm-6:nth-child(3) > .selectDiv > .champ-form__cm-input').click()
-   
-      cy.get('#modalBody > .form-row > .col-sm-6:nth-child(3) > .selectDiv > .champ-form__cm-input').select('HI')
-   
-      cy.get('#modalBody > .form-row > .col-sm-6:nth-child(3) > .selectDiv > .champ-form__cm-input').click()
-   
-      cy.get('form > #modalBody > .form-row > .col-lg-6:nth-child(4) > .champ-form__cm-input').click()
-   
-      cy.get('form > #modalBody > .form-row > .col-lg-6:nth-child(5) > .champ-form__cm-input').click()
-   
-      cy.get('#modalBody > .form-row > .px-1 > .selectDiv > .champ-form__cm-input').click()
-   
-      cy.get('#modalBody > .form-row > .px-1 > .selectDiv > .champ-form__cm-input').click()
+      cy.get('.d-flex > .d-flex > .position-relative > .d-flex > .button').click()
+      cy.get('#modalBody > .form-row > .form-group:nth-child(1) > .selectDiv > .champ-form__cm-input').select('RT')
+      cy.get('#modalBody > .form-row > .cstm-light-text:nth-child(3) > .selectDiv > .champ-form__cm-input').select('selfgen')
+      cy.get('form > #modalBody > .form-row > .cstm-light-text:nth-child(5) > .champ-form__cm-input').type('Automated')
+      cy.get('form > #modalBody > .form-row > .form-group:nth-child(6) > .champ-form__cm-input').type('Partner')
+      cy.get('form > #modalBody > .form-row > .form-group:nth-child(7) > .champ-form__cm-input').type('ap@mail.com')
+      cy.get('form > #modalBody > .form-row > .form-group:nth-child(8) > .champ-form__cm-input').type('(222) 222-2222')
+      cy.get('form > #modalBody > .form-row > .form-group:nth-child(12) > .champ-form__cm-input').type('BBB')
+      cy.get('.modal-box--dialog__container-footer > .w-100 > .position-relative:nth-child(3) > .button > span').click()
+      cy.get('#mloflo_body > #root > .Snackbar_snackbar-wrapper__ocbPJ > .Snackbar_snackbar__GsYZl > .Snackbar_snackbar__text__1Hx2a').click()
    
    })
   
